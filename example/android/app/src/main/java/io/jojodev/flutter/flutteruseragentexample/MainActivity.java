9577a67a34240a1ce2599caed1f223047cbe05ee
koga akito <koga.akito@eversense.co.jp>
2021-03-25 17:38:57 +0900

    update flutter embadded2

--- example/android/app/src/main/java/io/jojodev/flutter/flutteruseragentexample/MainActivity.java
+++ example/android/app/src/main/java/io/jojodev/flutter/flutteruseragentexample/MainActivity.java
@@ -1,8 +1,6 @@
 package io.jojodev.flutter.flutteruseragentexample;
 
-import android.os.Bundle;
-import io.flutter.app.FlutterActivity;
-import io.flutter.plugins.GeneratedPluginRegistrant;
+import io.flutter.embedding.android.FlutterActivity;
 
 public class MainActivity extends FlutterActivity {
   @Override
